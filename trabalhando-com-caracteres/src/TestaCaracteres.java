4897e2fb9a8c88c3988e41380ba5bb0a4aaa284f
Felipe Souto <68256101+LipeSou@users.noreply.github.com>
2020-07-31 22:41:15 -0300

    Atualizando

---
+++ trabalhando-com-caracteres/src/TestaCaracteres.java
@@ -0,0 +1,10 @@
+
+public class TestaCaracteres {
+	public static void main(String[] args) {
+		char letra = 'a'; //carrega uma letra
+		String palavra = "Teste de caracteres funcionando com String";
+		
+		System.out.println(letra);
+		System.out.println(palavra);
+	}
+}
